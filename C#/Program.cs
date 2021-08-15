40db4e069c1ca00836b426a81099f1c55eb52129
bruhsilian <77811343+FLight944@users.noreply.github.com>
2021-08-15 00:15:17 -0300

    Adicionada pastinha para baguncinhas em C# :3

---
+++ C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teste{
+    class Program{
+        public static void Pause(){
+            Console.WriteLine("Aperte ENTER para encerrar o programa");
+            Console.ReadLine();
+        }
+        static void Main(string[] args){
+            Console.WriteLine("Bom dia");
+            Pause();
+        }
+    }
+}
