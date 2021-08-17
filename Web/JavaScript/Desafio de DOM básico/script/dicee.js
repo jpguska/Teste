abb92be1985cb7cd15b48b8f08a839388c2f1caf
FLight944 <tamikomaconhada@gmail.com>
2021-08-17 02:48:48 -0300

    adicionado testes com DOM em Javascript, e preparado mini projetinho :3

---
+++ Web/JavaScript/Desafio de DOM básico/script/dicee.js
@@ -0,0 +1,1 @@
+alert("João Pedro é gay.");
---
+++ Web/JavaScript/script/DOMbs.js
@@ -0,0 +1,12 @@
+document.querySelector("h1").innerHTML = "João Pedro eu te odeio :´(";
+document.querySelector("a").style.color = "pink";
+document.querySelector("hr").style.fontSize = "10rem";
+document.querySelector("hr").style.color = "green"
+document.querySelector("h1").style.color = "cyan";
+
+function lul() {
+    document.querySelector("#agaum").classList.add("huge");
+    document.querySelector("h1#agaum.huge").textContent = "EU TE ODEIO MUITO JOÃO PEDRO >:(";
+    //EASTER EGG!!!
+    document.querySelector("a.jp").setAttribute("href", "https://youtu.be/d7BpBAL2ztc?t=5");
+}
