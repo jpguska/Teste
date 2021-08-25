38ad4ebbe2cef8d92e860ca5c24e8474834a593f
FLight944 <tamikomaconhada@gmail.com>
2021-08-24 23:27:03 -0300

    feito outro projetinho pessoal e rápido :)

--- Javascript/Nodejs/calcproj/calculator.js
+++ Javascript/Nodejs/calcproj/calculator.js
@@ -19,31 +19,40 @@ app.post("/bmicalculator", function(request, response){
     response.send(`Seu BMI é de: ${Number(request.body.weight) / (Number(request.body.height) * Number(request.body.height))}`)
 });
 
-app.get("/traitcalculator", function(request, response){
+app.get("/speccalculator", function(request, response){
     response.sendFile(`${__dirname}/html/traitcalc.html`);
 });
 
-app.post("/traitcalculator", function(request, response){
+app.post("/speccalculator", function(request, response){
     //HERE COMES DA LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOONG BOAH
-    let collection = calculate(Number(request.body.head))
-    response.send(`Head: ${Number(request.body.head).toFixed(2)}cm <br> Eye: ${collection[0].toFixed(2)}cm <br> Nose: ${collection[1].toFixed(2)}cm <br> Lips: ${collection[2].toFixed(2)}cm <br> Chin: ${collection[3].toFixed(2)}cm <br> Eye partition: ${(Number(request.body.eye) / 3).toFixed(2)}cm`);
+    let collection = calcTraited(Number(request.body.head))
+    response.send(`Head: ${Number(request.body.head).toFixed(2)}cm <br> Eye: ${collection[0].toFixed(2)}cm <br> Nose: ${collection[1].toFixed(2)}cm <br> Lips: ${collection[2].toFixed(2)}cm <br> Cheek: ${collection[3].toFixed(2)}cm <br> Eye partition: ${(Number(request.body.eye) / 3).toFixed(2)}cm`);
 
-})
+});
+app.get("/traditionalcalculator", function(request, response){
+    response.sendFile(`${__dirname}/html/traditionalcalc.html`);
+});
+
+app.post("/traditionalcalculator", function(request, response){
+    let division = (Number(request.body.head) / 2) / 2;
+    response.send(`Medida da cabeça: ${Number(request.body.head)} <br> Medida do meio ${Number(request.body.head / 2)} <br> Divisa do meio ${division} <br> <hr>`)
+});
+
 
 app.listen(3000, function(){
     console.log("Server online na porta: 3000");
-})
+});
 
 
 
 
 //functions
 
-function calculate(number){
+function calcTraited(number){
     let col = []
     col.push(number / 2);//eye
     col.push((number / 2)/2);//nose
     col.push(((number / 2)/2)/2);//lips
-    col.push(((((number/ 2)/2)/2)/2)); //chin
+    col.push(((((number/ 2)/2)/2)/2)); //cheek
     return col;
 }
