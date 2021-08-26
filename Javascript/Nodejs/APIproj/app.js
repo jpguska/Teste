7fe97e97fefb4af98ad5dcf7b3705acee898be18
FLight944 <tamikomaconhada@gmail.com>
2021-08-26 20:43:59 -0300

    adicionado alguns detalhes pro bagui

--- Javascript/Nodejs/APIproj/app.js
+++ Javascript/Nodejs/APIproj/app.js
@@ -6,11 +6,14 @@ const https = require("https");
 
 
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ecfee4a6c281059d4b07be78bc5f87db";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Brasilia&id=524901&appid=ecfee4a6c281059d4b07be78bc5f87db";
     https.get(url, function(response){
-        console.log(response);
+        console.log(response.statusCode);
+       response.on("data", function(data){
+           const localweather = JSON.parse(data);
+           res.send(`<h1>${MedidaDeSerHumano(localweather.main.temp)}Cº</h1> <br> ${localweather.weather[0].description}`);
+       }) 
     });
-    res.send("hi");
 })
 
 
@@ -18,3 +21,7 @@ app.get("/", function(req, res){
 app.listen(3000, function(){
     console.log("Server ligado na porta 3000. Acesse localhost:3000/ para acessar o servidor.");
 });
+
+function MedidaDeSerHumano(Fatreich){
+    return ((Fatreich - 32) / 1.8).toFixed(2);
+}
